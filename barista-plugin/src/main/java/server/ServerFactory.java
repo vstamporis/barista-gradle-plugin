ef42d73f4a37be5eaeb34e9a3d74fb428c20efd1
Stelios Tsiskomichelis <tsiskost@gmail.com>
2018-10-20 19:39:57 +0300

    Add REST service controller class

---
+++ barista-plugin/src/main/java/server/ServerFactory.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Tsiskomichelis Stelios
+ * Created On: 20/10/2018
+ * Project: butler-plugin
+ * <p>
+ * ClassName: ServerFactory
+ * Role:
+ * Description:
+ */
+package server;
+
+public class ServerFactory {
+}
