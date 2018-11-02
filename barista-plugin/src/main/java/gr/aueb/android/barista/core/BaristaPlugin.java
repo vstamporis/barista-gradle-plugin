7ef20ba9816347ffe77d22d6e2fec2391cfb7abb
Tsiskomichelis Stelios - Developer/Systems Analyst <s.tsisko@aldemarhotels.com>
2018-11-02 19:29:54 +0200

    Server shuts down by plugin after "connectedDebugAndroidTest" Task

--- barista-plugin/src/main/java/gr/aueb/android/barista/core/BaristaPlugin.java
+++ barista-plugin/src/main/java/gr/aueb/android/barista/core/BaristaPlugin.java
@@ -9,15 +9,24 @@
  */
 package gr.aueb.android.barista.core;
 //todo use gradle properties to inject variables to  BuildConfig.
+import gr.aueb.android.barista.server.HttpServerManager;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.logging.LogLevel;
 
+import java.util.Iterator;
+
 public class BaristaPlugin implements Plugin<Project> {
-    // todo check for assemble release test
-    private String TARGET_TASK = "assembleDebugAndroidTest";
+    //The android task that assembles the debug test app
+    private String ASSEMBLE_DEBUG_ANDROID_TEST = "assembleDebugAndroidTest";
+
+    // the android task that runs the android instrumented tests of the project
+    //private String CONNECTED_ANDROID_TEST = "connectedDebugAndroidTest";
+
+    //todo try this task for all cases 1 & 2
+    private String CONNECTED_ANDROID_TEST = "connectedDebugAndroidTest";
 
     public void apply(Project project){
 
@@ -28,15 +37,21 @@ public class BaristaPlugin implements Plugin<Project> {
                 @Override
                 public void execute(Project project) {
                     System.out.println("After Evaluation!");
-                    Task targetTask = project.getTasks().findByPath(TARGET_TASK);
+                    Iterator<Task> it = project.getTasks().iterator();
+                    while(it.hasNext()){
+                        System.out.println(">>: "+it.next().getName());
+                    }
+
+
+                    Task targetTask = project.getTasks().findByPath(ASSEMBLE_DEBUG_ANDROID_TEST);
                     if(targetTask == null ){
-                        project.getLogger().log(LogLevel.ERROR,"No Android task '"+TARGET_TASK+"' found");
+                        project.getLogger().log(LogLevel.ERROR,"No Android task '"+ASSEMBLE_DEBUG_ANDROID_TEST+"' found");
 
                     }
                     else{
-                        project.getLogger().log(LogLevel.ERROR,"Task  '"+TARGET_TASK+"' Found !!! ");
+                        project.getLogger().log(LogLevel.ERROR,"Task  '"+ASSEMBLE_DEBUG_ANDROID_TEST+"' Found !!! ");
                         deployDispatcherServer(targetTask,project);
-
+                        hookServerStopTask(project);
                     }
                 }
             });
@@ -45,19 +60,6 @@ public class BaristaPlugin implements Plugin<Project> {
         // The target project is NOT an android project
         else{
             System.out.println("This is not an Android Project");
-            /**
-             * TODO Delete all if noe an android Project. Only for testing purposes
-             * TESTING SECTION
-             * deploys either way a deployServer task just to be able to run on none android projects
-             */
-            project.getLogger().log(LogLevel.ERROR,"EXECUTING SOMETHING NOW");
-            project.getTasks().create("deployTestDispacherServerTask", TestDispacherServerTask.class,
-                    new Action<TestDispacherServerTask>() {
-                        @Override
-                        public void execute(TestDispacherServerTask testDispacherServerTask) {
-                            testDispacherServerTask.deployServer();
-                        }
-                    });
 
         }
 
@@ -86,14 +88,26 @@ public class BaristaPlugin implements Plugin<Project> {
         targetTask.doLast(new Action<Task>() {
             @Override
             public void execute(Task task) {
-                project.getLogger().log(LogLevel.ERROR,"EXECUTING SOMETHING NOW");
-                project.getTasks().create("deployTestDispacherServerTask", TestDispacherServerTask.class,
-                        new Action<TestDispacherServerTask>() {
-                            @Override
-                            public void execute(TestDispacherServerTask testDispacherServerTask) {
-                                testDispacherServerTask.deployServer();
-                            }
-                        });
+                System.out.println("[BARISTA]: Closing Server");
+                HttpServerManager.startServer();
+            }
+        });
+    }
+
+    // Jersey server should be automaticaly shut down when connectedAndroidTest task is completed
+    // todo There are two ways to run an  instrumented test. Must find how to close when using 2
+    //  1.  from command lind with 'gradle connectedAndroidTest'
+    //  2.  with adb using '$ adb shell am instrument -w <test_package_name>/<runner_class>'
+    // Curently the plugin shuts down the server if the first way is used.
+    private void hookServerStopTask(Project project){
+        System.out.println("[BARISTA] : Hook Server Stop Task");
+        Task targetTask = project.getTasks().findByPath(CONNECTED_ANDROID_TEST);
+        targetTask.doLast(new Action<Task>() {
+
+            @Override
+            public void execute(Task task) {
+                System.out.println("[BARISTA]: Closing Server");
+                HttpServerManager.stopServer();
             }
         });
     }
