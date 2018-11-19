2641427f44cb11aa43bff09a7d8c12309c79b6aa
Stelios Tsiskomichelis <tsiskost@gmail.com>
2018-11-19 15:44:36 +0200

    Improved BuildConfig injection for port configuration to avoid NotSerializableException

--- barista-plugin/src/main/java/gr/aueb/android/barista/core/BaristaPlugin.java
+++ barista-plugin/src/main/java/gr/aueb/android/barista/core/BaristaPlugin.java
@@ -11,10 +11,13 @@ package gr.aueb.android.barista.core;
 //todo use gradle properties to inject variables to  BuildConfig.
 import com.android.build.gradle.AndroidConfig;
 import com.android.build.gradle.BaseExtension;
+import com.android.build.gradle.LibraryExtension;
 import com.android.build.gradle.api.AndroidArtifactVariant;
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension;
 import com.android.builder.model.ClassField;
 import com.google.wireless.android.sdk.stats.AndroidView;
 import gr.aueb.android.barista.server.HttpServerManager;
+import groovy.lang.Closure;
 import org.glassfish.grizzly.utils.ArraySet;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
@@ -23,6 +26,7 @@ import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.logging.LogLevel;
+import org.gradle.api.tasks.TaskState;
 import com.android.build.gradle.api.BaseVariant;
 
 
@@ -42,6 +46,7 @@ public class BaristaPlugin implements Plugin<Project> {
 
     private final String ANDROID_EXTENSION_NAME = "android";
 
+
     private Project project;
 
     public void apply(Project project){
@@ -52,7 +57,7 @@ public class BaristaPlugin implements Plugin<Project> {
         // PLUGIN LOGIG
         // todo check for best practises and code style
         // check if target project is an android project
-        if(isAndroidProject(project)){
+        if(isAndroidProject()){
             System.out.println("This is an Android Project");
             project.afterEvaluate(new Action<Project>() {
 
@@ -67,9 +72,11 @@ public class BaristaPlugin implements Plugin<Project> {
                     }
                     else{
                         project.getLogger().log(LogLevel.ERROR,"Task  '"+ASSEMBLE_DEBUG_ANDROID_TEST+"' Found !!! ");
+                        // todo debug only
+                        scanProject();
                         hookPortConfiguration();
-                        deployDispatcherServer(targetTask,project);
-                        hookServerStopTask(project);
+                        deployDispatcherServer(targetTask);
+                        hookServerStopTask();
                     }
                 }
             });
@@ -87,21 +94,22 @@ public class BaristaPlugin implements Plugin<Project> {
     /**
      * Function that determins if target project is an android project based on the imported plugins
      * // todo find source
-     * @param project
+     *
      * @return true if is android project, false if not
      */
-    private static boolean isAndroidProject(final Project project) {
+    private boolean isAndroidProject() {
 
-        final boolean isAndroidLibrary = project.getPlugins().hasPlugin("com.android.library");
-        final boolean isAndroidApp = project.getPlugins().hasPlugin("com.android.application");
-        final boolean isAndroidTest = project.getPlugins().hasPlugin("com.android.test");
-        final boolean isAndroidFeature = project.getPlugins().hasPlugin("com.android.feature");
-        final boolean isAndroidInstantApp = project.getPlugins().hasPlugin("com.android.instantapp");
+        final boolean isAndroidLibrary = this.project.getPlugins().hasPlugin("com.android.library");
+        final boolean isAndroidApp = this.project.getPlugins().hasPlugin("com.android.application");
+        final boolean isAndroidTest = this.project.getPlugins().hasPlugin("com.android.test");
+        final boolean isAndroidFeature = this.project.getPlugins().hasPlugin("com.android.feature");
+        final boolean isAndroidInstantApp = this.project.getPlugins().hasPlugin("com.android.instantapp");
 
         return isAndroidLibrary || isAndroidApp || isAndroidTest || isAndroidFeature || isAndroidInstantApp;
+       //return isAndroidApp || isAndroidTest || isAndroidFeature || isAndroidInstantApp;
     }
 
-    private void deployDispatcherServer(Task targetTask, Project project){
+    private void deployDispatcherServer(Task targetTask){
 
         targetTask.doLast(new Action<Task>() {
             @Override
@@ -122,8 +130,10 @@ public class BaristaPlugin implements Plugin<Project> {
     //  1.  from command lind with 'gradle connectedAndroidTest'
     //  2.  with adb using '$ adb shell am instrument -w <test_package_name>/<runner_class>'
     // Curently the plugin shuts down the server if the first way is used.
-    private void hookServerStopTask(Project project){
+
+    private void hookServerStopTask(){
         System.out.println("[BARISTA] : Hook Server Stop Task");
+
         Task targetTask = project.getTasks().findByPath(CONNECTED_ANDROID_TEST);
         targetTask.doLast(new Action<Task>() {
 
@@ -133,63 +143,59 @@ public class BaristaPlugin implements Plugin<Project> {
                 HttpServerManager.stopServer();
             }
         });
+
+//        targetTask.configure(new Closure() {
+//        })
     }
+
+    //todo commented out due to serilization bug
 
     private void hookPortConfiguration(){
 
-        BaseExtension androidExtension= getAndroidExtension();
-        System.out.println("[BARISTA]: "+androidExtension.getCompileSdkVersion());
+        BaseAppModuleExtension androidExtension= getAndroidExtension();
+        if(androidExtension != null) {
+            androidExtension.getDefaultConfig()
+                    .buildConfigField("Integer","BARISTA_PORT",""+getProvidedPort()+"");
 
-        androidExtension.getDefaultConfig().addBuildConfigField(new ClassField() {
-            @Override
-            public String getType() {
-                return "Integer";
-            }
-
-            @Override
-            public String getName() {
-                return "BARISTA_PORT";
-            }
-
-            @Override
-            public String getValue() {
-                return ""+getProvidedPort()+"";
-            }
-
-            @Override
-            public String getDocumentation() {
-                return "Generated by Barista Plugin to populate the listening port of the server to the project";
-            }
-
-            @Override
-            public Set<String> getAnnotations() {
-                return new HashSet<String>();
-            }
-        });
+        }
 
     }
 
     /**
-     * Function that returns the android extension of the android project
+     * Function that returns the android extension of the android project.
+     *
      * @return
      */
-    private BaseExtension getAndroidExtension(){
-        Iterator<Project> it  = project.getAllprojects().iterator();
-        while(it.hasNext()){
-            System.out.println(it.next().getName());
+
+    private BaseAppModuleExtension getAndroidExtension(){
+        Object o = project.getExtensions().findByName(this.ANDROID_EXTENSION_NAME);
+
+        System.out.println("[BARISTA-PLUGIN]: Android Extension Class Name: "+o.getClass().getName());
+        if(o instanceof com.android.build.gradle.internal.dsl.BaseAppModuleExtension){
+            System.out.println("[BARISTA-PLUGIN]: INSTACE OF BaseAppModuleExtension");
+            return (BaseAppModuleExtension) project.getExtensions().findByName(this.ANDROID_EXTENSION_NAME);
         }
-        return (BaseExtension) this.project.getExtensions().findByName(this.ANDROID_EXTENSION_NAME);
+
+       return null;
+
     }
 
     /**
      *
      * @return
      */
-    private int getProvidedPort(){
+    private Integer getProvidedPort(){
         BaristaConfigurationExtension settings = project.getExtensions().findByType(BaristaConfigurationExtension.class);
         System.out.println("[BARISTA]: Found Port >> "+settings.getPort() );
         return settings.getPort();
     }
 
+
+    private void scanProject(){
+        int projectNum = project.getAllprojects().size();
+        System.out.println("[BARISTA-PLUGIN] Number of Projects: "+projectNum);
+
+
+    }
 
 }
